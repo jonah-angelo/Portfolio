6fd071afed7035080ded1205a10fce9289ca595b
jonah-angelo <jonah.angelo@gmail.com>
2023-05-02 15:33:34 -0700

    imported tailwind

---
+++ jonah-portfolio/src/components/App/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './styles.css'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '../HomePage'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ jonah-portfolio/src/components/HomePage/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export default function HomePage() {
+  return (
+    <div className="welcome-header">
+      <h1>Welcome to My Page!</h1>
+    </div>
+  );
+}
